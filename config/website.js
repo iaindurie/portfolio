bbccdc33ca0faf5e2c041c0227facc673f6e4adb
Iain Durie <>
2019-03-16 09:32:46 +0000

    Initial config

--- config/website.js
+++ config/website.js
@@ -1,13 +1,13 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Bella - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Bella', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-bella.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'Iain Durie - UX/UI Engineer', // Navigation and Site Title
+  siteTitleAlt: 'Iain Durie', // Alternative Site title for SEO
+  siteUrl: 'http://www.iaindurie.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'A bright single-page portfolio starter with big typography & images for Gatsby.',
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'Personal portfolio of work for Iain Durie',
+  author: 'Iain Durie', // Author for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@bella', // Twitter Username
